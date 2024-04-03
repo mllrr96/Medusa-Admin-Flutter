9cc091506f4e1b4eba90610bbc0e7a88e3ce01e6
Mohammed Ragheb <eellrraawwii@gmail.com>
2024-04-03 20:32:37 +0300

    Fix android gradle and manifest

--- android/app/src/main/kotlin/com/rag/medusa/admin/medusa_admin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.rag.medusa_admin
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
---
+++ android/app/src/main/kotlin/com/rag/medusa_admin_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rag.medusa_admin_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
